95f2868212261de90e77102a60b23a4aa00fc02a
lellon0403 <a67733198@gmail.com>
2025-09-02 21:34:18 +0900

    Fix: Correct typo in 'Hello, world!' message.

--- lib/week01/tutorial01.dart
+++ lib/week01/tutorial01.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(
     const Center(
       child: Text(
-      'Hello, world!',
-      textDirection: TextDriection.ltr,
-      style: TextStyle(fontSize: 32, color: Colors.white),
+        'Hello, wolrd!',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 32, color: Colors.white),
+      ),
     ),
-     ),   
-    ).
   );
 }
