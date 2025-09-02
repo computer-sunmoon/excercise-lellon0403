256f7ee3e48ce72e05772d505304c30af06e7440
lellon0403 <a67733198@gmail.com>
2025-09-02 21:31:19 +0900

    Fix: Corrected typo and styled 'Hello, World!' text

--- lib/week01/tutorial01.dart
+++ lib/week01/tutorial01.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 
 void main() {
   run App(constconst Center(
-    child: Text('Hello, World!') )
-  ))
+    child: Text(
+      'Hello, wordl!',
+      textDirection: TextDriection.ltr,
+      style: TextStyle(fontSize: 32, color: Colors.white),
+    ),
+     ),   
+    ).
+  );
 }
